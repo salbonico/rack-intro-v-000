f29237be6b9f7032f176e10163608d7ce31ce489
Steve Albonico <salbonico@gmail.com>
2019-02-01 19:06:15 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -2,9 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hi Jess! Your looking very beautiful today."
     resp.finish
   end
 
 end
-
